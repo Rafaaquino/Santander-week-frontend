5cb144691d2fd834af7dcff8ddb6b23f971e0acc
Rafael Aquino <rafaaquino28@gmail.com>
2021-06-13 21:54:38 -0300

    Stock card layout

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageComponent } from './page/page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
 
 
 
 @NgModule({
   declarations: [
     PageComponent,
+    StockCardComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    PageComponent
+    PageComponent,
+    StockCardComponent
   ],
 })
 export class DashboardModule { }
